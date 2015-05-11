e4d30b4cc289572fda1f5e4e69a2baa8e59faeb8
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2015-05-11 05:42:43 -0400

    functioning implementation of aspect-ratio preserving rectangle arrangement

---
+++ test_aspackt.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+from unittest import TestCase
+
+from aspackt import arrangement, AspectRatio, Coordinate
+
+Box = namedtuple('Box', ['width', 'height'])
+
+class AspacktTest(TestCase):
+    def test_approximates_aspect_ratio(self):
+        a = Box(6, 1)
+        b = Box(2, 6)
+        c = Box(3, 2)
+        d = Box(2, 4)
+
+        ratio = AspectRatio(4, 3)
+        arranged = arrangement([a,b,c,d], aspect_ratio=ratio)
+
+        self.assertEqual(Coordinate(0, 0), arranged[b])
+        self.assertEqual(Coordinate(2, 0), arranged[d])
+        self.assertEqual(Coordinate(4, 0), arranged[c])
+        self.assertEqual(Coordinate(4, 2), arranged[a])
